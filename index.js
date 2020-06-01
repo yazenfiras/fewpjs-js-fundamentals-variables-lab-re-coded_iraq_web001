7c97729fc763e636ae9f2dbe9ba82fa70ecc2b9e
yazen firas <psyazen3@gmail.com>
2020-06-01 23:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const companyName='Scuber';
 let mostProfitableNeighborhood='Chelsea';
+<<<<<<< HEAD
 let companyCeo='Susan Smith';
+=======
+let 
+>>>>>>> 3d4e311910566dbb135d47819fe415d0d4c8c191
 
 
